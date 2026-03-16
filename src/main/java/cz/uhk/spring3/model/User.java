683cf6bca2ebe39efe95d6bb52252a0ed55abf7f
pallado1 <dominik.palla@uhk.cz>
2026-03-16 12:06:35 +0100

    New field - User.email.

--- src/main/java/cz/uhk/spring3/model/User.java
+++ src/main/java/cz/uhk/spring3/model/User.java
@@ -10,6 +10,15 @@ public class User {
     private int id;
     private String name;
     private String password;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public int getId() {
         return id;
